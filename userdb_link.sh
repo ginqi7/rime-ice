322023d8d14567af70d89588953827b0c141e982
Qiqi Jin <ginqi7@gmail.com>
2025-06-24 12:38:08 +0800

    Forcefully delete rime_ice.userdb.

--- userdb_link.sh
+++ userdb_link.sh
@@ -1,2 +1,2 @@
-rm "$HOME/Library/Rime/rime_ice.userdb"
+rm -rf "$HOME/Library/Rime/rime_ice.userdb"
 ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Rime/rime_ice.userdb" "$HOME/Library/Rime/rime_ice.userdb"
